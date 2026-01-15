cafa2bdb7b0d3340fa8dc4306ffb9bc5f162fbc5
ladyofcode <thelab@ladyofcode.com>
2026-01-15 20:13:15 +1100

    fix static deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,10 @@
-// import adapter from '@sveltejs/adapter-auto';
-// import adapter from '@sveltejs/adapter-static';
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
-		// adapter-node allows prerendering most pages while server-rendering dynamic ones
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			out: 'build',
 			precompress: false
